2faa6bacbb7de2da96a70a3032e61e741718112b
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-01-22 11:52:59 +0900

    fix : 닉네임 영어 입력값 바꾸기

--- src/main/java/com/allo/server/domain/user/dto/request/NicknameRequest.java
+++ src/main/java/com/allo/server/domain/user/dto/request/NicknameRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record NicknameRequest(@Pattern(regexp = "^[가-힣0-9]*$", message = "닉네임은 한글, 숫자만 사용가능합니다.")
-                              @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+public record NicknameRequest(@NotBlank(message = "닉네임은 필수 입력 값입니다.")
                               @Size(min=2, max=10, message = "닉네임은 2~10자로 입력해 주세요.")String nickname) {
 }
