9ad1f3aa265a5ee456342d942050754253321e4e
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-01-24 10:36:58 +0900

    feat : 유저 정보 Language 추가 구현

---
+++ src/main/java/com/allo/server/domain/user/entity/Language.java
@@ -0,0 +1,20 @@
+package com.allo.server.domain.user.entity;
+
+import com.allo.server.error.exception.custom.BadRequestException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import static com.allo.server.error.ErrorCode.UNKNOWN_LECTURE_LANGUAGE;
+
+public enum Language {
+    ENGLISH, CHINESE, JAPANESE, VIETNAMESE; // ENGLISH : 영어, CHINESE : 중국어, JAPANESE : 일본어, VIETNAMESE : 베트남어
+
+    @JsonCreator // 이 어노테이션은 Jackson이 JSON에서 객체로 변환할 때 사용
+    public static Language fromString(String value) {
+        for (Language type : Language.values()) {
+            if (type.name().equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new BadRequestException(UNKNOWN_LECTURE_LANGUAGE);
+    }
+}
