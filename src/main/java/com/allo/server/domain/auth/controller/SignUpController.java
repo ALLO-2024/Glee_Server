513ab532123d36f76c54a230c20c7bcc9b5b596d
seokhee <skdlzl753@naver.com>
2024-05-12 11:20:33 +0900

    docs: swagger 설정

--- src/main/java/com/allo/server/domain/auth/controller/SignUpController.java
+++ src/main/java/com/allo/server/domain/auth/controller/SignUpController.java
@@ -25,15 +25,23 @@ public class SignUpController {
     private final AuthService authService;
 
     @Operation(summary = "회원가입")
-    @PostMapping("/users/signUp")
-    public ResponseEntity<BaseResponse> userSignUp(@RequestPart (value = "UserSignUpRequest") UserSignUpRequest request, @RequestPart(value = "file", required = false) MultipartFile multipartFile) throws IOException {
+    @PostMapping(value = "/users/signUp",
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BaseResponse> userSignUp(
+        @RequestPart (value = "UserSignUpRequest") UserSignUpRequest request,
+        @RequestPart(value = "file", required = false) MultipartFile multipartFile) throws IOException {
         authService.userSignUp(request, multipartFile);
         return ResponseEntity.ok(new BaseResponse<>(SUCCESS));
     }
 
     @Operation(summary = "소셜 로그인 회원가입")
-    @PatchMapping("/users/signUp/social")
-    public ResponseEntity<BaseResponse> userSocialSignUp(@AuthenticationPrincipal UserDetails loginUser,@Valid @RequestPart (value = "SocialSignUpRequest") SocialSignUpRequest socialSignUpRequest, @RequestPart(value = "file", required = false) MultipartFile multipartFile) throws IOException {
+    @PatchMapping(value = "/users/signUp/social",
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BaseResponse> userSocialSignUp(
+        @AuthenticationPrincipal UserDetails loginUser,
+        @Valid @RequestPart (value = "SocialSignUpRequest") SocialSignUpRequest socialSignUpRequest,
+        @RequestPart(value = "file", required = false) MultipartFile multipartFile) throws IOException {
+
         authService.userSocialSignUp(loginUser.getUsername(), socialSignUpRequest, multipartFile);
         return ResponseEntity.ok(new BaseResponse<>(SUCCESS));
     }
