bb1c6adaf13b5ae12efaf8e8c3e67995bc5752a4
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-06-04 19:53:51 +0900

    fix : 단어 검색 수정

--- src/main/java/com/allo/server/domain/word/dto/response/WordSearchResponse.java
+++ src/main/java/com/allo/server/domain/word/dto/response/WordSearchResponse.java
@@ -1,13 +1,14 @@
 package com.allo.server.domain.word.dto.response;
 
-public record WordSearchResponse(String word, String meaning, String pos, String trans_word, String example, Boolean exist) {
+public record WordSearchResponse(String word, String meaning, String pos, String trans_word, String example, Boolean exist, Long wordId) {
 
-    public WordSearchResponse(String word, String meaning, String pos, String trans_word, String example, Boolean exist) {
+    public WordSearchResponse(String word, String meaning, String pos, String trans_word, String example, Boolean exist, Long wordId) {
         this.word = word;
         this.meaning = meaning;
         this.pos = pos;
         this.trans_word = trans_word;
         this.example = example;
         this.exist = exist;
+        this.wordId = wordId;
     }
 }
