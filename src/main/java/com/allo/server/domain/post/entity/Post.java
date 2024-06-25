ea7c2b4ea8ba2f933b9394b28a9ea6dca347832d
hseoky desktop <skdlzl753@naver.com>
2024-06-26 08:48:58 +0900

    post 엔티티 생성

---
+++ src/main/java/com/allo/server/domain/post/entity/Post.java
@@ -0,0 +1,37 @@
+package com.allo.server.domain.post.entity;
+
+import com.allo.server.domain.user.entity.UserEntity;
+import com.allo.server.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Post extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long postId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private UserEntity userEntity;
+
+  private String title;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+}
