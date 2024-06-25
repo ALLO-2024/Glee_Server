8b0e4a7a76af8a5d4c7a442ea5458be69212a3b1
hseoky desktop <skdlzl753@naver.com>
2024-06-26 08:54:51 +0900

    PostImage 엔티티 생성

---
+++ src/main/java/com/allo/server/domain/post_image/entity/postImage.java
@@ -0,0 +1,34 @@
+package com.allo.server.domain.post_image.entity;
+
+
+import com.allo.server.domain.post.entity.Post;
+import com.allo.server.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class postImage extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long postImageId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private Post post;
+
+  @Column(nullable = false)
+  private String postImageUrl;
+}
