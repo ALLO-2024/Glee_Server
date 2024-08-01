5208d1625f417bec2bbd0898543e7a2150787e73
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2024-08-01 23:57:07 +0900

    fix : 좋아요 수정

--- src/main/java/com/allo/server/domain/post_like/controller/PostLikeController.java
+++ src/main/java/com/allo/server/domain/post_like/controller/PostLikeController.java
@@ -4,6 +4,7 @@ import com.allo.server.domain.post_like.dto.request.PostLikeRequest;
 import com.allo.server.domain.post_like.dto.response.PostLikeResponse;
 import com.allo.server.domain.post_like.service.PostLikeService;
 import com.allo.server.global.entity.BaseEntity;
+import com.allo.server.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.allo.server.response.BaseResponseStatus.SUCCESS;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post-like")
@@ -21,12 +24,11 @@ public class PostLikeController {
   private final PostLikeService postLikeService;
 
   @PostMapping("/toggle")
-  public ResponseEntity<PostLikeResponse> postLikeToggle(
+  public ResponseEntity<BaseResponse> postLikeToggle(
       @AuthenticationPrincipal UserDetails loginUser,
       @RequestBody PostLikeRequest postLikeRequest) {
 
-    PostLikeResponse postLikeResponse = postLikeService.postLikeToggle(loginUser.getUsername(),
-        postLikeRequest.getPostId());
-    return ResponseEntity.ok(postLikeResponse);
+    PostLikeResponse postLikeResponse = postLikeService.postLikeToggle(loginUser.getUsername(), postLikeRequest.getPostId());
+    return ResponseEntity.ok(new BaseResponse<>(SUCCESS));
   }
 }
